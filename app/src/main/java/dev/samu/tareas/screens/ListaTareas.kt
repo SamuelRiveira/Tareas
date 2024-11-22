90a7463ef066434e2e22aad6d77cd9938ef7a47f
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-22 10:30:07 +0000

    Añadido boton flotante

--- app/src/main/java/dev/samu/tareas/screens/ListaTareas.kt
+++ app/src/main/java/dev/samu/tareas/screens/ListaTareas.kt
@@ -1,6 +1,7 @@
 package dev.samu.tareas.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,11 +9,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.samu.tareas.data.Task
@@ -21,39 +28,53 @@ import dev.samu.tareas.viewmodel.TaskViewModel
 
 @Composable
 fun ListaTareas(navController: NavHostController, taskViewModel: TaskViewModel) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Button(onClick = {
-            taskViewModel.addTask(Task(title = "Nueva Tarea", content = "Contenido de prueba"))
-        }) {
-            Text(text = "Agregar tarea")
-        }
+    Box{
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Button(onClick = {
+                taskViewModel.addTask(Task(title = "Nueva Tarea", content = "Contenido de prueba"))
+            }) {
+                Text(text = "Agregar tarea")
+            }
 
-        LazyColumn {
-            items(taskViewModel.task) { task ->
-                Column(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
-                    Text(text = "Título: ${task.title}")
-                    Text(text = "Contenido: ${task.content}")
-                    Row {
-                        Button(onClick = { taskViewModel.deleteTask(task) }) {
-                            Text(text = "Eliminar")
-                        }
-                        Button(onClick = {
-                            val updatedTask = task.copy(content = "Contenido actualizado")
-                            taskViewModel.updateTask(updatedTask)
-                        }) {
-                            Text(text = "Editar")
+            LazyColumn {
+                items(taskViewModel.task) { task ->
+                    Column(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+                        Text(text = "Título: ${task.title}")
+                        Text(text = "Contenido: ${task.content}")
+                        Row {
+                            Button(onClick = { taskViewModel.deleteTask(task) }) {
+                                Text(text = "Eliminar")
+                            }
+                            Button(onClick = {
+                                val updatedTask = task.copy(content = "Contenido actualizado")
+                                taskViewModel.updateTask(updatedTask)
+                            }) {
+                                Text(text = "Editar")
+                            }
                         }
                     }
                 }
             }
-        }
 
-        Button(onClick = { navController.navigate(route = AppScreens.Tarea.route) }) {
-            Text(text = "Ir a la segunda pantalla")
+            Button(onClick = { navController.navigate(route = AppScreens.Tarea.route) }) {
+                Text(text = "Ir a la segunda pantalla")
+            }
+        }
+        FloatingActionButton(
+            onClick = {
+
+            },
+            containerColor = Color(0xffFFA500),
+            contentColor = Color.White,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(24.dp)
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Crear")
         }
     }
 }
