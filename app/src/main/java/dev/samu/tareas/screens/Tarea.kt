7e40d02702ccdef131bc34891b12ff200c7f260d
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-25 12:08:20 +0000

    Actualizado imports

--- app/src/main/java/dev/samu/tareas/screens/Tarea.kt
+++ app/src/main/java/dev/samu/tareas/screens/Tarea.kt
@@ -2,22 +2,19 @@ package dev.samu.tareas.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import dev.samu.tareas.navigation.AppScreens
 import dev.samu.tareas.viewmodel.TaskViewModel
 
 @Composable
@@ -32,8 +29,8 @@ fun Tarea(navController: NavHostController, taskViewModel: TaskViewModel, indice
                 .padding(top = 16.dp)
             ) {
                 Text(
-                    text = task.title,
-                    modifier = Modifier
+                    text = "${task.title}",
+                    fontSize = 20.sp
                 )
                 Text(text = "Contenido: ${task.content}")
 //                Row {
