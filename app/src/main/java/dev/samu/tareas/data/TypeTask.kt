8716037e63cc1310ca58bdeae8854d954ca173cb
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-26 12:16:14 +0000

    Añadido tipo tarea

---
+++ app/src/main/java/dev/samu/tareas/data/TypeTask.kt
@@ -0,0 +1,10 @@
+package dev.samu.tareas.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "TiposTareas")
+data class TipoTarea(
+    @PrimaryKey(autoGenerate = true) val idTipoTarea: Int = 0,
+    val tituloTipoTarea: String
+)
