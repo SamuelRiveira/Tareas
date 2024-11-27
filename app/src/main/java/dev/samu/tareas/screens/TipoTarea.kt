777b3e164b344a26a1ba5a31b7dbfb255edb951b
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-27 09:26:04 +0000

    Añadido boton de ver tipos

--- app/src/main/java/dev/samu/tareas/screens/TipoTarea.kt
+++ app/src/main/java/dev/samu/tareas/screens/TipoTarea.kt
@@ -3,15 +3,26 @@ package dev.samu.tareas.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -30,14 +41,20 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import dev.samu.tareas.data.Task
+import dev.samu.tareas.data.TypeTask
 import dev.samu.tareas.navigation.AppScreens
 import dev.samu.tareas.viewmodel.TypeTaskViewModel
+import java.lang.reflect.Type
+import kotlin.math.exp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TipoTarea(navController: NavHostController, typeTaskViewModel: TypeTaskViewModel) {
-    val selectedTask = typeTaskViewModel.typeTaskList
-    var textoTitulo by remember { mutableStateOf(typeTaskViewModel.typeTaskList.title ?: "") }
+
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+    var textoTitulo by remember { mutableStateOf("Tipo") }
+    var expanded by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -51,25 +68,50 @@ fun TipoTarea(navController: NavHostController, typeTaskViewModel: TypeTaskViewM
                 .padding(top = 16.dp)
         ) {
             Column {
-                TextField(
-                    value = textoTitulo,
-                    onValueChange = { textoTitulo = it },
-                    textStyle = TextStyle(
-                        fontSize = 30.sp,
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    ),
-                    colors = TextFieldDefaults.textFieldColors(
-                        containerColor = Color.Black,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
-                    ),
-                )
+                Row {
+                    TextField(
+                        value = textoTitulo,
+                        onValueChange = { textoTitulo = it }
+                    )
+                    Button(
+                        onClick = {
+                            expanded = true
+                        }
+                    ) {
+                        Text(text = "ver tipos")
+                    }
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ){
+                    typeTaskViewModel.typeTaskList.forEach { tipo ->
+                        DropdownMenuItem(
+                            text = {
+                                Text(text = "${tipo.name}")
+                            },
+                            onClick = {
+                                expanded = false
+                            }
+                        )
+                    }
+                }
             }
         }
 
-        val currentRoute =
-            navController.currentBackStackEntryAsState().value?.destination?.route
+        FloatingActionButton(
+            onClick = {
+                typeTaskViewModel.addTypeTask(TypeTask(name = textoTitulo))
+            },
+            containerColor = Color(0xffFFA500),
+            contentColor = Color.White,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(45.dp)
+                .padding(bottom = 95.dp)
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Crear")
+        }
 
         BottomAppBar(
             modifier = Modifier
